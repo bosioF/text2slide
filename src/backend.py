366044ebba19b5214c7363f54ebdc60471053beb
Federico Bosio <federicobosio0209@gmail.com>
2025-08-02 13:52:18 +0200

    cleaned main.py

--- src/backend.py
+++ src/backend.py
@@ -75,33 +75,54 @@ def process_video(input_file, color, output_file_name, quality, dir_name, audio_
     add_audio_to_video(final_path, audio_path)
     print(f"\nVideo generated at: {final_path}")
 
-def check_input():
-    if len(sys.argv) == 2 and sys.argv[1] == "--help":
-        print(HELP_MSG)
-        sys.exit(0)
+def check_input(color='', res=''):
+    if not color:
+        if len(sys.argv) == 2 and sys.argv[1] == "--help":
+            print(HELP_MSG)
+            sys.exit(0)
+        if len(sys.argv) not in [4, 5]:
+            print(USAGE_MSG)
+            print("\nOptionally, provide an MP3 path as a 4th argument.")
+            sys.exit(1)
 
-    if len(sys.argv) not in [4, 5]:
-        print(USAGE_MSG)
-        print("\nOptionally, provide an MP3 path as a 4th argument.")
-        sys.exit(1)
+        input_file = sys.argv[1]
+        quality = sys.argv[2]
+        output_file_name = sys.argv[3]
+        audio_path = sys.argv[4] if len(sys.argv) == 5 else None
 
-    input_file = sys.argv[1]
-    quality = sys.argv[2]
-    output_file_name = sys.argv[3]
-    audio_path = sys.argv[4] if len(sys.argv) == 5 else None
+        if quality not in ['-ql', '-qm', '-qh', '-qp', '-qk']:
+            print('Quality options: -ql (low), -qm (medium), -qh (high), -qp (2K), -qk (4K)')
+            sys.exit(1)
+        if output_file_name.startswith('.'):
+            print('Invalid output file name')
+            sys.exit(1)
 
-    if quality not in ['-ql', '-qm', '-qh', '-qp', '-qk']:
-        print('Quality options: -ql (low), -qm (medium), -qh (high), -qp (2K), -qk (4K)')
-        sys.exit(1)
+        allowed_chars = string.ascii_letters + string.digits + "-_"
+        if any(c not in allowed_chars for c in output_file_name):
+            print("Invalid output file name. Only letters, digits, '-' and '_' allowed.")
+            sys.exit(1)
 
-    if output_file_name.startswith('.'):
-        print('Invalid output file name')
-        sys.exit(1)
+        return input_file, quality, output_file_name, audio_path
+    elif color:
+        if color not in COLOR_NAMES:
+            print(f'{color} is not an available color')
+            sys.exit(1)
 
-    allowed_chars = string.ascii_letters + string.digits + "-_"
-    if any(c not in allowed_chars for c in output_file_name):
-        print("Invalid output file name. Only letters, digits, '-' and '_' allowed.")
-        sys.exit(1)
+        quality_to_dirName = {
+            "-ql": "480p15",
+            "-qm": "720p60",
+            "-qh": "1080p60",
+            "-qp": "1440p60",
+            "-qk": "2160p60"
+        }
 
-    return input_file, quality, output_file_name, audio_path
+        dir_name = quality_to_dirName.get(res)
+
+        if not os.path.exists("manim_files"):
+            os.mkdir("manim_files")
+        if not os.path.exists("output"):
+            os.mkdir("output")
+
+        return dir_name
+    return None
 
--- src/main.py
+++ src/main.py
@@ -1,29 +1,8 @@
 from backend import *
-from audio import *
 
 if __name__ == "__main__":
     input_file, quality, output_file_name, audio_path = check_input()
-
     color = input("What color should the text be?").strip().upper()
-
-    if color not in COLOR_NAMES:
-        print(f'{color} is not an available color')
-        sys.exit(1)
-
-    quality_to_dirName = {
-        "-ql": "480p15",
-        "-qm": "720p60",
-        "-qh": "1080p60",
-        "-qp": "1440p60",
-        "-qk": "2160p60"
-    }
-
-    dir_name = quality_to_dirName.get(quality)
-
-    if not os.path.exists("manim_files"):
-        os.mkdir("manim_files")
-    if not os.path.exists("output"):
-        os.mkdir("output")
-
+    dir_name = check_input(color, quality)
     process_video(input_file, color, output_file_name, quality, dir_name, audio_path)
 
